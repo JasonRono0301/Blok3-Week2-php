648cbe91d194cfdcc781794790257fbe9913d27d
Jason <99057964@mydavinci.nl>
2020-02-21 12:24:37 +0100

    Nog werken aan de eind opdracht.

---
+++ Blok3-week2-opdracht/madlibs.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Krona+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <div class="container">
+        <p>Mad Libs</p>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    
+</body>
+</html>
